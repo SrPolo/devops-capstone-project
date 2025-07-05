17ea79c833a9b8a1ecdefbfd86d53b42cd590b6c
SrPolo <juanpablopolancor22@gmail.com>
2025-07-05 00:01:36 -0400

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -11,7 +11,7 @@ from unittest import TestCase
 from tests.factories import AccountFactory
 from service import talisman
 from service.common import status  # HTTP Status Codes
-from service.models import db, Account, init_db, DataValidationError
+from service.models import db, Account, init_db
 from service.routes import app
 
 DATABASE_URI = os.getenv(
